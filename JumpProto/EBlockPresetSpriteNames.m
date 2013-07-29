92e98a1b1dbee71063f7e39a069ca0aed9d7f66b
gideon-ios <ggoodwin37@gmail.com>
2013-07-28 23:39:54 -0700

    Adding edithint versions of some sprites.

--- JumpProto/EBlockPresetSpriteNames.m
+++ JumpProto/EBlockPresetSpriteNames.m
@@ -108,8 +108,8 @@
         case EBlockPreset_tiny_cr_1: return @"tiny-cr-1";
         case EBlockPreset_tiny_cr_2: return @"tiny-cr-2";
         case EBlockPreset_tiny_bigcr: return @"tiny-bigcr";
-        case EBlockPreset_tiny_conveyor_l: return @"tiny-conveyor-0";  // TODO: should make icons for these
-        case EBlockPreset_tiny_conveyor_r: return @"tiny-conveyor-2";
+        case EBlockPreset_tiny_conveyor_l: return @"tiny-conveyor-l-edithint";
+        case EBlockPreset_tiny_conveyor_r: return @"tiny-conveyor-r-edithint";
         case EBlockPreset_tiny_oneway_u: return @"tiny-oneway-u";
         case EBlockPreset_tiny_oneway_l: return @"tiny-oneway-l";
         case EBlockPreset_tiny_oneway_r: return @"tiny-oneway-r";
@@ -123,9 +123,9 @@
         case EBlockPreset_tiny_bl_wallJump: return @"tiny-bl-walljump";
         case EBlockPreset_tiny_bl_exit: return @"tiny-exit";
         case EBlockPreset_tiny_lift: return @"tiny-lift-0";
-        case EBlockPreset_tiny_mv_plat_l: return @"tiny-mv-plat-0";  // TODO: icon for these
-        case EBlockPreset_tiny_mv_plat_r: return @"tiny-mv-plat-1";
-        case EBlockPreset_tiny_btn1: return @"tiny-light1-on";  // TODO icon
+        case EBlockPreset_tiny_mv_plat_l: return @"tiny-mv-plat-l-edithint";
+        case EBlockPreset_tiny_mv_plat_r: return @"tiny-mv-plat-r-edithint";
+        case EBlockPreset_tiny_btn1: return @"tiny-button-edithint";
         case EBlockPreset_tiny_sprtiny_0: return @"tiny-sprtiny-0";
         case EBlockPreset_tiny_sprtiny_1: return @"tiny-sprtiny-1";
         case EBlockPreset_tiny_sprtiny_2: return @"tiny-sprtiny-2";
@@ -133,7 +133,7 @@
         case EBlockPreset_tiny_pipe: return @"tiny-pipe-ud";
         case EBlockPreset_tiny_pipe_bub: return @"tiny-pipe-bub";
         case EBlockPreset_tiny_creep_fuzz_l: return @"tiny-creep-fuzz-0";
-        case EBlockPreset_tiny_creep_fuzz_r: return @"tiny-creep-fuzz-1";  // TODO: add a flipped sprite for use here
+        case EBlockPreset_tiny_creep_fuzz_r: return @"tiny-creep-fuzz-reversed";
         case EBlockPreset_tiny_creep_martian: return @"tiny-creep-martian-0";
         case EBlockPreset_tiny_creep_mosquito: return @"tiny-creep-mosquito-0";
         case EBlockPreset_tiny_creep_jelly: return @"tiny-creep-jelly-0";
