7bf22335235f8dd7e8a0610b1551825af4a99af7
gideon-ios <ggoodwin37@gmail.com>
2013-08-19 21:07:49 -0700

    Make ColorRectLayerView work correctly.

--- JumpProto/ColorRectLayerView.m
+++ JumpProto/ColorRectLayerView.m
@@ -9,15 +9,46 @@
 #import "ColorRectLayerView.h"
 
 @interface ColorRectLayerView (private)
-
-
 @end
 
 @implementation ColorRectLayerView
 
 
--(void)buildScene
+-(void)setupView
 {
+    glMatrixMode( GL_PROJECTION );
+    glLoadIdentity();
+    glOrthof( 0.f, 1.f, 0.f, 1.f, -1.f, 1.f );
+    glMatrixMode( GL_MODELVIEW );
+    glLoadIdentity();
+    
+    glDisable( GL_TEXTURE_2D );
+    glEnableClientState( GL_COLOR_ARRAY );
+    glEnableClientState( GL_VERTEX_ARRAY );
+    
+    glDisable( GL_BLEND );
+}
+
+-(void)buildScene
+{//  ll lr tl tr
+	static GLbyte ColorRectLayerView_colorData[] = {
+		0x11, 0x11, 0x50, 0xff,
+		0x20, 0x20, 0x60, 0xff,
+		0x00, 0x00, 0x10, 0xff,
+		0x00, 0x00, 0x08, 0xff
+	};
+	
+	static float ColorRectLayerView_vertData[] = {
+		0,0,0,
+		1,0,0,
+		0,1,0,
+		1,1,0,
+	};
+    
+	[self setupView];
+	glVertexPointer(3, GL_FLOAT, 0, ColorRectLayerView_vertData);
+	glColorPointer(4, GL_UNSIGNED_BYTE, 0, ColorRectLayerView_colorData );
+	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 }
 
 
