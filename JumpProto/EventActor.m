d2e539e8095270757e03f5cbb6ed374fd9c268d7
gideon-ios <ggoodwin37@gmail.com>
2013-09-01 23:44:38 -0700

    Fixes for button resetting.

--- JumpProto/EventActor.m
+++ JumpProto/EventActor.m
@@ -48,35 +48,38 @@
 
 -(void)spawnActorBlocks
 {
-    // each button owns four blocks: the bottom, the stopper, the trigger, and the plate.
-    // some values have an offset of 1 so that the trigger block doesn't get fouled up on the level block beneath the button.
-    
+    // some values have an offset so that the trigger block doesn't get fouled up on the level block beneath the button.
+    // TODO: have a feeling I'm just off-by-a-quarter somewhere in the sublayout. smaller values don't retrig.
+    const Emu foulOffset = ONE_BLOCK_SIZE_Emu / 4;
+
     EmuPoint bottomPoint = EmuPointMake( m_startingPoint.x + (3 * ONE_BLOCK_SIZE_Emu / 2), m_startingPoint.y - (3 * ONE_BLOCK_SIZE_Emu) );
+    EmuSize bottomSize = EmuSizeMake( 1 * ONE_BLOCK_SIZE_Emu, 1 * ONE_BLOCK_SIZE_Emu );
     m_bottomBlock = [[ActorBlock alloc] initAtPoint:bottomPoint];
-    m_bottomBlock.state.d = EmuSizeMake( 1 * ONE_BLOCK_SIZE_Emu, 1 * ONE_BLOCK_SIZE_Emu );
+    m_bottomBlock.state.d = bottomSize;
     m_bottomBlock.owningActor = self;
     m_bottomBlock.props.solidMask = BlockEdgeDirMask_Up;
-    //m_bottomBlock.defaultSpriteState = nil;  // bottom block doesn't have a visual representation.
-    m_bottomBlock.defaultSpriteState = [[[StaticSpriteState alloc] initWithSpriteName:@"tiny-btn1-plate"] autorelease];  // TEST
+    m_bottomBlock.defaultSpriteState = nil;  // bottom block doesn't have a visual representation.
+    //m_bottomBlock.defaultSpriteState = [[[StaticSpriteState alloc] initWithSpriteName:@"tiny-btn1-plate"] autorelease];  // TEST
     [m_actorBlockList addObject:m_bottomBlock];
     [m_world.elbowRoom addBlock:m_bottomBlock];
 
-    EmuPoint stopperPoint = EmuPointMake( m_startingPoint.x + ONE_BLOCK_SIZE_Emu, m_startingPoint.y - ONE_BLOCK_SIZE_Emu );
+    EmuPoint stopperPoint = EmuPointMake( m_startingPoint.x + ONE_BLOCK_SIZE_Emu, m_startingPoint.y - ONE_BLOCK_SIZE_Emu - foulOffset );
+    EmuSize stopperSize = EmuSizeMake( 2 * ONE_BLOCK_SIZE_Emu, 1 * ONE_BLOCK_SIZE_Emu );
     m_stopperBlock = [[ActorBlock alloc] initAtPoint:stopperPoint];
-    m_stopperBlock.state.d = EmuSizeMake( 2 * ONE_BLOCK_SIZE_Emu, 1 * ONE_BLOCK_SIZE_Emu - 1 );
+    m_stopperBlock.state.d = stopperSize;
     m_stopperBlock.owningActor = self;
     m_stopperBlock.props.canMoveFreely = YES;
     m_stopperBlock.props.weight = BUTTON_STOPPER_WEIGHT;
-    m_stopperBlock.props.initialVelocity = EmuPointMake( 0, 0 );
-    m_stopperBlock.props.solidMask = BlockEdgeDirMask_Up | BlockEdgeDirMask_Down;
-    //m_stopperBlock.defaultSpriteState = nil;  // stopper block doesn't have a visual representation.
-    m_stopperBlock.defaultSpriteState = [[[StaticSpriteState alloc] initWithSpriteName:@"tiny-btn1-plate"] autorelease];  // TEST
+    m_stopperBlock.props.bounceFactor = 1.f;  // don't switch directions on bounce.
+    m_stopperBlock.defaultSpriteState = nil;  // stopper block doesn't have a visual representation.
+    //m_stopperBlock.defaultSpriteState = [[[StaticSpriteState alloc] initWithSpriteName:@"tiny-btn1-plate"] autorelease];  // TEST
     [m_actorBlockList addObject:m_stopperBlock];
     [m_world.elbowRoom addBlock:m_stopperBlock];
     
-    EmuPoint triggerPoint = EmuPointMake( m_startingPoint.x + (3 * ONE_BLOCK_SIZE_Emu / 2), m_startingPoint.y - 1 );
+    EmuPoint triggerPoint = EmuPointMake( m_startingPoint.x + (3 * ONE_BLOCK_SIZE_Emu / 2), m_startingPoint.y - foulOffset );
+    EmuSize triggerSize = EmuSizeMake( 1 * ONE_BLOCK_SIZE_Emu, 2 * ONE_BLOCK_SIZE_Emu );
     m_triggerBlock = [[ActorBlock alloc] initAtPoint:triggerPoint];
-    m_triggerBlock.state.d = EmuSizeMake( 1 * ONE_BLOCK_SIZE_Emu, 2 * ONE_BLOCK_SIZE_Emu + 1 );
+    m_triggerBlock.state.d = triggerSize;
     m_triggerBlock.owningActor = self;
     m_triggerBlock.props.canMoveFreely = YES;
     m_triggerBlock.props.weight = BUTTON_TRIGGER_WEIGHT;
@@ -86,9 +89,10 @@
     [m_actorBlockList addObject:m_triggerBlock];
     [m_world.elbowRoom addBlock:m_triggerBlock];
 
-    EmuPoint platePoint = EmuPointMake( m_startingPoint.x + (1 * ONE_BLOCK_SIZE_Emu), m_startingPoint.y - 1 );
+    EmuPoint platePoint = EmuPointMake( m_startingPoint.x + (1 * ONE_BLOCK_SIZE_Emu), m_startingPoint.y - foulOffset );
+    EmuSize plateSize = EmuSizeMake( 2 * ONE_BLOCK_SIZE_Emu, 1 * ONE_BLOCK_SIZE_Emu + foulOffset );
     m_plateBlock = [[ActorBlock alloc] initAtPoint:platePoint];
-    m_plateBlock.state.d = EmuSizeMake( 2 * ONE_BLOCK_SIZE_Emu, 1 * ONE_BLOCK_SIZE_Emu + 1 );
+    m_plateBlock.state.d = plateSize;
     m_plateBlock.owningActor = self;
     m_plateBlock.defaultSpriteState = [[[StaticSpriteState alloc] initWithSpriteName:@"tiny-btn1-plate"] autorelease];
     [m_actorBlockList addObject:m_plateBlock];
@@ -100,7 +104,7 @@
 -(void)onBorn
 {
     [self spawnActorBlocks];
-    m_lifeState = ActorLifeState_Alive;  // actor remains alive indefinitely, even when the actorBlock is temporarily gone.
+    m_lifeState = ActorLifeState_Alive;  // actor remains alive indefinitely.
 }
 
 
@@ -109,45 +113,43 @@
 {
     [super updateControlStateWithTimeDelta:delta];
     
-    if( m_currentState == TinyBtn1State_Resting || m_currentState == TinyBtn1State_Trigging )
+    if( m_currentState == TinyBtn1State_Resting || m_currentState == TinyBtn1State_Trigging || m_currentState == TinyBtn1State_Resetting )
     {
         return;
     }
-
-    if( m_currentState == TinyBtn1State_Triggered )
-    {
-        // TODO: check if trigger block has any up abutters still, and if not, go to resetting state.
-        return;
-    }
+    NSAssert( m_currentState == TinyBtn1State_Triggered, @"Assume we must be in triggered state since we already checked the others." );
     
-    NSAssert( m_currentState == TinyBtn1State_Resetting, @"Assume we must be in resetting state since we already checked the others." );
-
-    // TODO (if we don't need any special handling for resetting, move the resetting state check to the no-op at top)
+    NSArray *abutters = [m_world.frameCache lazyGetAbuttListForSO:m_triggerBlock inER:m_world.elbowRoom direction:ERDirUp];
+    if( [abutters count] == 0 )
+    {
+        [self onUntriggered];
+        m_currentState = TinyBtn1State_Resetting;
+        [self updateForCurrentState];
+    }
 }
 
 
 -(void)updateForCurrentState
 {
-    if( m_currentState == TinyBtn1State_Resting )
+    const Emu stopperV = 1600;
+    if( m_currentState == TinyBtn1State_Resting || m_currentState == TinyBtn1State_Triggered )
     {
         return;
     }
-    if( m_currentState == TinyBtn1State_Trigging )
+    else if( m_currentState == TinyBtn1State_Trigging )
     {
-        // set stopper block to down velocity.
-        const Emu stopperDown = -1600;
-        [m_stopperBlock setV:EmuPointMake( 0, stopperDown )];
-        return;
+        [m_stopperBlock setV:EmuPointMake( 0, -stopperV )];
     }
-    if( m_currentState == TinyBtn1State_Triggered )
+    else if( m_currentState == TinyBtn1State_Resetting )
     {
-        // TODO
+        [m_stopperBlock setV:EmuPointMake( 0, stopperV )];
     }
-    if( m_currentState == TinyBtn1State_Resetting )
+    else
     {
-        // TODO
+        NSAssert( NO, @"What state is this?" );
     }
 }
+
 
 -(void)onTriggered
 {
@@ -156,41 +158,46 @@
 }
 
 
+-(void)onUntriggered
+{
+    // TODO: event system
+}
+
+
 // override
 -(void)collidedInto:(NSObject<ISolidObject> *)other inDir:(ERDirection)dir actorBlock:(ActorBlock *)origActorBlock
 {
     [super collidedInto:other inDir:dir actorBlock:origActorBlock];
-    if( m_currentState != TinyBtn1State_Resting && m_currentState != TinyBtn1State_Trigging )
+    if( m_currentState == TinyBtn1State_Triggered )
     {
-        // only care about collisions if we are resting or trigging.
-        return;
-    }
-    if( dir != ERDirUp )
-    {
-        // only care about up collisions (things landing on us).
-        // TODO: this assumes an up-facing button.
+        // don't care about collisions if we are in triggered stated.
         return;
     }
     
     if( m_currentState == TinyBtn1State_Resting )
     {
-        if( origActorBlock != m_triggerBlock )
+        if( dir != ERDirUp || origActorBlock != m_triggerBlock )
         {
-            // in resting state, only listen for collisions on the trigger.
             return;
         }
         m_currentState = TinyBtn1State_Trigging;
     }
     else if( m_currentState == TinyBtn1State_Trigging )
     {
-        if( origActorBlock != m_bottomBlock || other != m_stopperBlock )
+        if( dir != ERDirUp || origActorBlock != m_bottomBlock || other != m_stopperBlock )
         {
-            // in trigging state, only listen for collisions of the stopper onto the bottom blocker.
             return;
         }
-        // hook into the event system.
         [self onTriggered];
         m_currentState = TinyBtn1State_Triggered;
+    }
+    else if( m_currentState == TinyBtn1State_Resetting )
+    {
+        if( dir != ERDirDown || origActorBlock != m_plateBlock || other != m_stopperBlock )
+        {
+            return;
+        }
+        m_currentState = TinyBtn1State_Resting;
     }
 
     // handle the details of this actor's behavior.
