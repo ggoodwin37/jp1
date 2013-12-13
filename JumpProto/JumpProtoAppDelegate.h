6df1847bd0baa24f40fa21b915e1f2db1884b725
gideon-ios <ggoodwin37@gmail.com>
2013-12-12 22:37:54 -0800

    Narrowing some dependencies.

--- JumpProto/JumpProtoAppDelegate.h
+++ JumpProto/JumpProtoAppDelegate.h
@@ -7,8 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-
-@class JumpProtoLaunchViewController;
+#import "IParentChildVC.h"  // IAppStartStop
 
 @interface JumpProtoAppDelegate : NSObject <UIApplicationDelegate> {
 
@@ -16,6 +15,6 @@
 
 @property (nonatomic, retain) IBOutlet UIWindow *window;
 
-@property (nonatomic, retain) IBOutlet JumpProtoLaunchViewController *viewController;
+@property (nonatomic, retain) IBOutlet UIViewController<IAppStartStop> *viewController;
 
 @end
--- JumpProto/JumpProtoAppDelegate.m
+++ JumpProto/JumpProtoAppDelegate.m
@@ -8,8 +8,6 @@
 
 #import "JumpProtoAppDelegate.h"
 
-#import "JumpProtoLaunchViewController.h"
-
 @implementation JumpProtoAppDelegate
 
 
