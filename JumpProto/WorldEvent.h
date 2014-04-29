624d68d31611b89382c982f4bf09198a2ac2ecbf
gideon-ios <ggoodwin37@gmail.com>
2014-04-28 23:23:24 -0700

    WIP

--- JumpProto/WorldEvent.h
+++ JumpProto/WorldEvent.h
@@ -8,6 +8,60 @@
 
 #import <Foundation/Foundation.h>
 
+enum WorldEventTypeEnum
+{
+    WEPressed,
+    WEDown,
+    WEUp,
+    WECount,
+};
+typedef enum WorldEventTypeEnum WorldEventType;
+
+
+enum WorldEventFXTypeEnum
+{
+    WFXNone,
+    WFXTest,
+    WFXCount,
+};
+typedef enum WorldEventFXTypeEnum WorldEventFXType;
+
+
+// WorldEvent ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 @interface WorldEvent : NSObject
 
+-(id)initWithTargetId:(NSString *)targetIdIn type:(WorldEventType)typeIn;
+
+@property (nonatomic, retain) NSString *targetId;
+@property (nonatomic, assign) WorldEventType type;
+
+
 @end
+
+
+// WorldEventFX //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// describes an effect that can be triggered by an event.
+@interface WorldEventFX : NSObject
+
+-(id)initWithFXId:(WorldEventFXType)fxTypeIn params:(NSDictionary *)paramsIn;
+
+@property (nonatomic, assign) WorldEventFXType type;
+@property (nonatomic, retain) NSDictionary *params;
+
+@end
+
+
+// WorldEventHandler ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+@protocol WorldEventHandler <NSObject>
+
+-(void)onWorldEvent:(WorldEvent *)event;
+
+@end
+
+
+// TODO: button creates and fires an event. it knows its targetId (set at load time).
+//       it fires an event at an NSObject<WorldEventHandler>
+//       central event dispatcher handles this. then relays them to any blocks registered as listeners for this event.
--- JumpProto/WorldEvent.m
+++ JumpProto/WorldEvent.m
@@ -8,6 +8,55 @@
 
 #import "WorldEvent.h"
 
+// WorldEvent ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 @implementation WorldEvent
 
+@synthesize targetId, type;
+
+-(id)initWithTargetId:(NSString *)targetIdIn type:(WorldEventType)typeIn
+{
+    if( self = [super init] )
+    {
+        self.targetId = targetIdIn;
+        self.type = typeIn;
+    }
+    return self;
+}
+
+
+-(void)dealloc
+{
+    self.targetId = nil;
+    [super dealloc];
+}
+
+
 @end
+
+
+// WorldEventFX //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+@implementation WorldEventFX
+
+@synthesize type, params;
+
+-(id)initWithFXId:(WorldEventFXType)fxTypeIn params:(NSDictionary *)paramsIn
+{
+    if( self = [super init] )
+    {
+        self.type = fxTypeIn;
+        self.params = paramsIn;
+    }
+    return self;
+}
+
+
+-(void)dealloc
+{
+    self.params = nil;
+    [super dealloc];
+}
+
+@end
+
