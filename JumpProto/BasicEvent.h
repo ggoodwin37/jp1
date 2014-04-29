a25474acfef5ec64615c854c6c29ef836c3cd858
gideon-ios <ggoodwin37@gmail.com>
2014-04-28 22:38:04 -0700

    Initial commit for events.

---
+++ JumpProto/BasicEvent.h
@@ -0,0 +1,13 @@
+//
+//  BasicEvent.h
+//  JumpProto
+//
+//  Created by Gideon Goodwin on 4/28/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BasicEvent : NSObject
+
+@end
---
+++ JumpProto/BasicEvent.m
@@ -0,0 +1,13 @@
+//
+//  BasicEvent.m
+//  JumpProto
+//
+//  Created by Gideon Goodwin on 4/28/14.
+//
+//
+
+#import "BasicEvent.h"
+
+@implementation BasicEvent
+
+@end
